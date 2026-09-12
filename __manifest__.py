f907af82166933f6e69658ae7a0f1518ad5a471c
MathysDC <mad@omydoo.fr>
2026-09-12 13:37:04 +0200

    Add __manifest__.py for OMD Validation Probe module

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "OMD Validation Probe",
+    "version": "19.0.1.0.0",
+    "summary": "Module sonde, sans effet fonctionnel.",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
